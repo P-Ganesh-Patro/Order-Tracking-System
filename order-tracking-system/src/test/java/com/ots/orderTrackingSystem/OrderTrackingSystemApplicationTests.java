b3043c85244b416ccfe400e0c0ccf8e2654430ee
ganesh <pganeshpatro1999@gmail.com>
2025-05-26 13:51:49 +0530

    Initial commit

---
+++ order-tracking-system/src/test/java/com/ots/orderTrackingSystem/OrderTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ots.orderTrackingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
