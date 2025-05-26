452d98c91a59ba1311023cfc44a3a980e10c472f
ganesh <pganeshpatro1999@gmail.com>
2025-05-26 17:18:00 +0530

    run changes

--- order-tracking-system/src/main/java/com/ots/orderTrackingSystem/OrderTrackingSystemApplication.java
+++ order-tracking-system/src/main/java/com/ots/orderTrackingSystem/OrderTrackingSystemApplication.java
@@ -9,6 +9,8 @@ public class OrderTrackingSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OrderTrackingSystemApplication.class, args);
 		System.out.println("hello");
+		System.out.println("new model");
+
 	}
 
 }
