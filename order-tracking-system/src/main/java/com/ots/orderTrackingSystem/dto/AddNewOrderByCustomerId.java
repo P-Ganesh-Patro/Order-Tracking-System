170840853f16b00dc51fa6d4d1984863fd33044f
ganesh <pganeshpatro1999@gmail.com>
2025-06-02 10:28:36 +0530

    new-changes-controllers

---
+++ order-tracking-system/src/main/java/com/ots/orderTrackingSystem/dto/AddNewOrderByCustomerId.java
@@ -0,0 +1,30 @@
+package com.ots.orderTrackingSystem.dto;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class AddNewOrderByCustomerId {
+
+    @NotNull(message = "Customer Id is required")
+    private Long customerId;
+
+    @NotEmpty(message = "Product List cannot be empty")
+    private List<ProductQuantity> products;
+
+    @Data
+    public static class ProductQuantity {
+
+        @NotNull(message = "Product Id is required")
+        private Long ProductId;
+
+        @Min(value = 1, message = "Quantity must be at least 1")
+        private int quantity;
+
+    }
+
+}
---
+++ order-tracking-system/src/main/java/com/ots/orderTrackingSystem/dto/OrderItemRequest.java
@@ -0,0 +1,6 @@
+package com.ots.orderTrackingSystem.dto;
+
+public class OrderItemRequest {
+    private Long productId;
+    private Long quantity;
+}
